0f38cf4b11e783aa9627f55a3d8f811940c0ab4a
Anrit Bhatt <anritbhatt2312@icloud.com>
2026-07-01 11:35:13 +0530

    Fix NextAuth: disable PKCE, use state-only check for Netlify

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -3,29 +3,11 @@ import GoogleProvider from 'next-auth/providers/google'
 
 const handler = NextAuth({
   secret: process.env.NEXTAUTH_SECRET,
-  useSecureCookies: true,
-  cookies: {
-    pkceCodeVerifier: {
-      name: 'next-auth.pkce.code_verifier',
-      options: {
-        httpOnly: true,
-        sameSite: 'none',
-        path: '/',
-        secure: true,
-      },
-    },
-  },
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-      authorization: {
-        params: {
-          prompt: 'consent',
-          access_type: 'offline',
-          response_type: 'code',
-        },
-      },
+      checks: ['state'],
     }),
   ],
   callbacks: {
